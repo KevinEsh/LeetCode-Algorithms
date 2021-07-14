3a93e933ef409234a8d2c66bc56dc96c88e0deed
KevinEsh <kevin_esh@outlook.com>
2021-07-14 17:12:49 -0500

    1929 solved

---
+++ 1900-2000/1929/main.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> getConcatenation(vector<int> &nums)
+    {
+        int size = nums.size();
+        for (int i = 0; i < size; i++)
+        {
+            nums.push_back(nums[i]);
+        }
+        return nums;
+    }
+};
