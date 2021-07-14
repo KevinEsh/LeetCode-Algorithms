613dc360270630d013b305fe4829151832e14a00
KevinEsh <kevin_esh@outlook.com>
2021-07-14 18:28:39 -0500

    1480 solved

---
+++ 1400-1500/1480/main.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> runningSum(vector<int> &nums)
+    {
+        int size = nums.size();
+        for (int i = 1; i < size; i++)
+        {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+};
