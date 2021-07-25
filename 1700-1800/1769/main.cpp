9c2e68d492cf4dd70c9c73e60a5b16d7b3f329da
Kevin Esh <52937054+KevinEsh@users.noreply.github.com>
2021-07-25 18:58:32 -0500

    1769 solved

---
+++ 1700-1800/1769/main.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+        int n=  boxes.size();
+        vector<int> answer(n);
+        
+        for (int i=0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if (i== j){
+                    continue;
+                }
+                
+                if (boxes[j] == '1'){
+                    answer[i] += abs(j -i);
+                }
+                
+            }
+        }
+        
+        return answer;
+        
+    }
+};
